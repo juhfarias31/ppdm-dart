5e2a0e4d2217772128c5deb0c56bba650b4337d8
Julia Farias de Queiroz <133894616+juhfarias31@users.noreply.github.com>
2024-03-20 15:39:33 -0300

    Add files via upload

---
+++ a8/Polimorfismo.dart
@@ -0,0 +1,28 @@
+import 'Casses.dart';
+void main(){
+
+  Lobo lobo = new Lobo();
+  lobo.emitirSom();
+  lobo.nome;
+  lobo.sexo;
+  lobo.raca;
+
+  Leao leao = new Leao();
+  leao.emitirSom();
+  leao.nome;
+  leao.sexo;
+  leao.raca;
+
+  Cachorro cachorro = new Cachorro();
+  cachorro.emitirSom();
+  cachorro.nome;
+  cachorro.sexo;
+  cachorro.raca;
+
+  Gato gato = new Gato();
+  gato.emitirSom();
+  gato.nome;
+  gato.sexo;
+  gato.raca;
+  
+}
